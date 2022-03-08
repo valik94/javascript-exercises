fb284b7635eb643ec1017bb2edeb0517ff407470
Valentin <valentin.lyash@gmail.com>
2022-03-07 22:56:45 -0500

    solved exercise 2 repeating strings

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, numTimes) {
+    // console.log(`string is`, str)
+    // console.log(`numTimes is`,numTimes)
+    if (numTimes < 0){
+        return 'ERROR'
+    }
+    const result = str.repeat(numTimes)
+    console.log(`result of repeat is`, result);
+    return result;
 };
 
 // Do not edit below this line
