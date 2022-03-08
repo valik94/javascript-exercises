9d4a7b38131ec780e7faf63e5c7d35e271f2e79f
Valentin <valentin.lyash@gmail.com>
2022-03-07 23:04:29 -0500

    solved reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(origString) {
+    let newString =''
+    console.log(`orignstring is `, origString)
+    for (let i=origString.length-1; i>=0; i--) {
+        console.log(`original letter is:`, origString[i])
+        newString = newString+origString[i]
+    }
+    console.log(`FINAL string is:`, newString)
+    return newString
 };
 
 // Do not edit below this line
